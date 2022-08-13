c4d270945991da4542e3919f2d535ea4379030c6
youdaehoon <107843412+youdaehoon@users.noreply.github.com>
2022-08-13 17:21:50 +0900

    수빈님 login axios전 병합

---
+++ src/components/MyCalendar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Calendar from "react-calendar";
+import styled from "styled-components";
+import "../css(subin)/Calendar.css";
+
+const MyCalendar = () => {
+  const [value, onChange] = React.useState(new Date());
+  React.useEffect(() => {
+    console.log(value);
+  }, [value]);
+  return (
+    <CalendarContainer>
+      <Calendar
+        calendarType="US"
+        locale="en-GB"
+        onChange={onChange}
+        value={value}
+      />
+    </CalendarContainer>
+  );
+};
+
+export default MyCalendar;
+
+const CalendarContainer = styled.div`
+  /* ~~~ container styles ~~~ */
+
+`;
--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,54 +1,101 @@
 import Stomp from "stompjs";
 import SockJS from "sockjs-client";
 import React from "react";
+import styled from "styled-components";
 //https://github.com/spring-guides/gs-messaging-stomp-websocket/blob/main/complete/src/main/resources/static/app.js 참고
 
 var stompClient = null;
 const Chat = () => {
-  const [isConnected,setIsConnected]=React.useState(false);
-  const [showMessage,setShowMessage]=React.useState([]);
-  const [sendMessage,setSendMessage]=React.useState("");
-  const [sendNick,setSendNick]=React.useState("");
+  const [isConnected, setIsConnected] = React.useState(false);
+  const [showMessage, setShowMessage] = React.useState([]);
+  const [sendMessage, setSendMessage] = React.useState("");
+  const [sendNick, setSendNick] = React.useState("");
 
-  function connect(){
-    var socket = new SockJS('/gs-guide-websocket');
+React.useState(()=>{
+  console.log(showMessage)
+},[showMessage])
+
+  function connect() {
+    var socket = new SockJS("http://13.125.116.193/ws/chat");
     stompClient = Stomp.over(socket);
-    stompClient.connect({},connected)
-
+    // stompClient.debug=null;
+    stompClient.connect({}, connected);
   }
-  function connected(){
+  function connected() {
     setIsConnected(true);
-    stompClient.subscribe('/topic/greetings',subscribed);
+    stompClient.subscribe("/topic", subscribed);
   }
-  function subscribed(greeting){
-    console.log(JSON.parse(greeting.body).content)//채팅 보내면 채팅을 뿌려줄것이다!
-   
+  function subscribed(greeting) {
+    console.log("여기안와?")
+    const soketMessage=JSON.parse(greeting.body)
+    
+    const newMessage=showMessage.push(soketMessage)
+
+    setShowMessage(prev=>newMessage) 
 
   }
 
   function disconnect() {
     if (stompClient !== null) {
-        stompClient.disconnect();
+      stompClient.disconnect();
     }
     setIsConnected(false);
     console.log("Disconnected");
-}
-function sendMessageFN(){
+  }
+  function sendNicknameFN() {}
+
+  function sendMessageFN() {
+    try {
+      stompClient.send(
+        "/app/chat/message",
+        {},
+        JSON.stringify({
+          message: sendMessage,
+          sender: sendNick,
+          // type:"aa",
+          // roomId:"aa",
   
-}
-function sendNicknameFN(){
-  stompClient.send("/app/hello", {}, JSON.stringify({'name': sendNick}));// 채팅을 보낸다.
-}
+        })
+      );
+    } catch (error) {
+      console.log(error)
+      
+    }
+     // 채팅을 보낸다.
+  }
   return (
     <div>
       <button onClick={connect}>연결!</button>
       <button onClick={disconnect}>소켓 연결 끊기!</button>
       <hr></hr>
-      <input placeholder="nickname을 입력하세요" onChange={(e)=>{setSendNick(e.target.value)}}></input><button onClick={sendNicknameFN}>닉네임등록</button>
+      <input
+        placeholder="nickname을 입력하세요"
+        onChange={(e) => {
+          setSendNick(e.target.value);
+        }}
+      ></input>
+      <button onClick={sendNicknameFN}>닉네임등록</button>
       <hr></hr>
-      <input placeholder="message을 입력하세요" onChange={(e)=>{setSendMessage(e.target.value);console.log(sendMessage)}}></input><button onClick={sendMessageFN}>메시지보내기</button>
+      <input
+        placeholder="message을 입력하세요"
+        onChange={(e) => {
+          setSendMessage(e.target.value);
+        }}
+      ></input>
+      <button onClick={sendMessageFN}>메시지보내기</button>
+      <hr/>
+      <ContainerMessage >
+      {showMessage.map((v,i)=>{return <div key={i}>{v.sender}:{v.message}</div>})}
+      </ContainerMessage>
+      
 
     </div>
-  )
-}
-export default Chat
+  );
+};
+export default Chat;
+
+
+const ContainerMessage=styled.div`
+display: flex;
+flex-direction:column;
+`
