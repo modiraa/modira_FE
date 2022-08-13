f6c8a61b96072f41297f581d5f7f69285f7a420e
whtnqls124578 <whtnqls124578@naver.com>
2022-08-13 17:52:28 +0900

    axios전 합치기

--- src/pages/MyInfo.js
+++ src/pages/MyInfo.js
@@ -1,6 +1,38 @@
+import '../css(subin)/MyInfo.css';
+
+import React, { useState } from 'react';
+
 const MyInfo = () => {
+
+  const [infoUserName, setInfoUserName] = useState('');
+
+  React.useEffect(()=>{
+
+  },[])
+
   return (
-    <div>MyInfo</div>
+    <div className='my_info'>
+      <div className='info_wrap'>
+        <div className='info_user_img'>
+          <img src='https://cdn-icons-png.flaticon.com/512/14/14660.png?w=360'/>
+        </div>
+        <div className='info_user_name'>
+          <h3>김영희</h3>
+        </div> 
+        <div className='info_user_gender'>
+          <h3>여성</h3>
+        </div>
+        <div className='info_user_age'>
+          <h3>20대</h3>
+        </div>
+        <div className='info_user_good'>
+          <span>12</span>
+        </div>
+        <div className='info_user_update_btn'>
+          <button>프로필 수정</button>
+        </div>
+      </div>
+    </div>
   )
 }
 export default MyInfo
