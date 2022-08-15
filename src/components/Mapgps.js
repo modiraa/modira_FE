4b1c05f7d341eccd39accd234932e525f2b60945
whtnqls124578 <whtnqls124578@naver.com>
2022-08-15 17:09:15 +0900

    8월 15일 중간 업데이트

--- src/components/Mapgps.js
+++ src/components/Mapgps.js
@@ -54,7 +54,7 @@ const Mapgps = () => {
                 infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
                 infowindow.open(map, marker);
                 console.log(place)// 수빈님이 이용할 수 있는 정보
-                navigate('/Write',{state:{juso: place.address_name,}})
+                navigate('/Write',{state:{juso: place.address_name,x : place.x, y : place.y}})
             });
         }
 
