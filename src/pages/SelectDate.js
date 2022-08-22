3cf387c1fee19b8559e4084eb393f0393a126917
whtnqls124578 <whtnqls124578@naver.com>
2022-08-22 16:45:15 +0900

    카카오 세션스토리지 추가

--- src/pages/SelectDate.js
+++ src/pages/SelectDate.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import '../css(subin)/WriteSelect.css';
+import '../css(subin)/SelectDate.css';
 import MyCalendar from '../components/MyCalendar';
 
-const WriteSelect = () => {
+const SelectDate = () => {
 
     return (
-        <div className='write_select'>
-            <div className='WriteSelect_top'>
-                <div className='write_page_count'>
+        <div className='select-date'>
+            <div className='select-date-top'>
+                <div className='select-date-count'>
                     <p>1/6</p>
                 </div>
-                <div className='write_top_title'>
+                <div className='select-date-title'>
                     <h2>모임을 원하는<br/>날짜를 알려주세요</h2>
                 </div>
-                <div className='write_top_value'>
+                <div className='select-date-value'>
                     <h3>2022/02/21</h3>
                     <h5>에 만나는 모임이에요</h5>
                 </div>
@@ -21,23 +21,18 @@ const WriteSelect = () => {
             <div className='date'>
                 <MyCalendar/>
             </div>
-            
-            <div className='WriteSelect_bottom'>
-                {/* <div className='prev'>
-                    <span className="material-symbols-outlined">
-                        arrow_back_ios
-                    </span>
-                    <h4>이전</h4>
-                </div> */}
+            <div className='select-date-bottom'>
                 <div className='next'>
                     <h4>다음</h4>
-                    <span className="material-symbols-outlined">
-                        arrow_forward_ios
-                    </span>
+                    <div className='date-arrow-forward-ios'>
+                        <span className="material-symbols-outlined">
+                            arrow_forward_ios
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
     )
 }
 
-export default WriteSelect;
+export default SelectDate;
