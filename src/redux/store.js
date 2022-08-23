af87db3a72919ed16368fc5a1976000048494c7c
whtnqls124578 <whtnqls124578@naver.com>
2022-08-23 18:59:30 +0900

    네비게이트 데이터 넘겨주기

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+//store는 Reducer을 뭉친 rootReducer로 만든다.
+
+import {createStore, combineReducers} from "redux";
+import userName from "./moduls/UserName.js"; // 리듀셔 가져오고
+
+const rootReducer = combineReducers({userName}); //리듀서들을 묶어준다.(지금은 하나밖에 없어서 하나만)
+
+// rootReducer랑 createStore 엮어서 store 만들어줌
+const store = createStore(rootReducer);
+
+export default store;
