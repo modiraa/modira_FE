b4efc4a1770048b9dd9b1a64d09aae3447d4bc89
youdaehoon <107843412+youdaehoon@users.noreply.github.com>
2022-09-02 15:16:11 +0900

    prevent conflict file useprofile

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,4 +1,4 @@
-import Stomp,{connect} from "stompjs";
+import Stomp, { connect } from "stompjs";
 import SockJS from "sockjs-client";
 import React from "react";
 import styled from "styled-components";
@@ -7,12 +7,11 @@ import { BiExit } from "react-icons/bi";
 import "../css(subin)/chat.css";
 import MessagelList from "../components/MessagelList";
 import UserProfile from "./UserProfile";
-import MyCalendar from"../components/MyCalendar"
+import MyCalendar from "../components/MyCalendar";
 import MessageInput from "../components/MessageInput";
 import MyModal from "../components/MyModal";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-
 
 //https://github.com/spring-guides/gs-messaging-stomp-websocket/blob/main/complete/src/main/resources/static/app.js 참고
 
@@ -23,47 +22,52 @@ const Chat = () => {
   const [sendMessage, setSendMessage] = React.useState("");
   const [sendNick, setSendNick] = React.useState("");
   const [enterChatRoom, setEnterChatRoom] = React.useState([]);
-  const [modalIsopen,setmodalIsopen]=React.useState(false);
-  const [postId,setPostId]=React.useState("");
-  const [chatRoomId,setChatRoomId]=React.useState("");
+  const [modalIsopen, setmodalIsopen] = React.useState(false);
+  const [postId, setPostId] = React.useState("");
+  const [chatRoomId, setChatRoomId] = React.useState("");
   const RefViewControll = React.useRef();
- const navigate=useNavigate();
+  const navigate = useNavigate();
 
- React.useEffect(()=>{
-  var socket = new SockJS("http://52.79.223.9/ws-stomp");
-  stompClient = Stomp.over(socket);
-  // stompClient.debug=null;
-  stompClient.connect({}, connected);
-
- },[])
   React.useEffect(() => {
-    console.log(showMessage)
-  
+    var socket = new SockJS("http://52.79.223.9/ws-stomp");
+    stompClient = Stomp.over(socket);
+    // stompClient.debug=null;
+    stompClient.connect(
+      {
+        Authorization: `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjb3PthqDtgbAiLCJleHAiOjE2NjIwOTMyMjksInVzZXJuYW1lIjoiS2FrYW9uYW1lMjQxMDkwMzEyOSJ9.Y9N_2aYxLDmglG50dZdd8XbXd1Xevp52Eu3sZ3GfzZ0TFLUZqKvR0Q0SPh35v7iJCMS17EFOHqKJdJFLAmdvdg`,
+      },
+      connected
+    );
+  }, []);
+  React.useEffect(() => {
+    console.log(showMessage);
+
     //가장 최근 채팅 보여주기
     if (RefViewControll.current) {
       RefViewControll.current.scrollTop = RefViewControll.current.scrollHeight;
     }
-  }, [showMessage, enterChatRoom,sendMessage,chatRoomId]);
+  }, [showMessage, enterChatRoom, sendMessage, chatRoomId]);
 
-  const modalHandler=()=>{
+  const modalHandler = () => {
     setmodalIsopen(true);
-  }
-  const handleClickCancel = () => {
-    setmodalIsopen(false)
   };
-
+  const handleClickCancel = () => {
+    setmodalIsopen(false);
+  };
 
   function connected() {
     setIsConnected(true);
-    console.log(chatRoomId)
-    if(chatRoomId){
-      stompClient.subscribe(`/sub/chat/room/1d9af0fe-119c-46c6-b75b-c56e64a26928`, subscribed);
+    console.log(chatRoomId);
+    if (chatRoomId) {
+      stompClient.subscribe(
+        `/sub/chat/room/22f09ce6-cb44-4f16-a054-20f8a09f5275`,
+        subscribed
+      );
     }
-    
   }
   //
   function subscribed(greeting) {
-    console.log("여기올텐데")
+    console.log("여기올텐데");
     const soketMessage = JSON.parse(greeting.body);
     showMessage.push(soketMessage);
     setShowMessage([...showMessage]);
@@ -80,16 +84,20 @@ const Chat = () => {
   }
   function sendNicknameFN() {
     try {
-      console.log(chatRoomId)
+      let auth = `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjb3PthqDtgbAiLCJleHAiOjE2NjIwOTMyMjksInVzZXJuYW1lIjoiS2FrYW9uYW1lMjQxMDkwMzEyOSJ9.Y9N_2aYxLDmglG50dZdd8XbXd1Xevp52Eu3sZ3GfzZ0TFLUZqKvR0Q0SPh35v7iJCMS17EFOHqKJdJFLAmdvdg`;
+      let sendmessage = JSON.stringify({
+        // message: sendMessage,
+        sender: sendNick,
+        type: "ENTER",
+        roomId: "22f09ce6-cb44-4f16-a054-20f8a09f5275",
+      });
+      console.log(chatRoomId);
       stompClient.send(
         "/pub/chat/message",
-        {},
-        JSON.stringify({
-          // message: sendMessage,
-          sender: sendNick,
-          type: "ENTER",
-          roomId:"1d9af0fe-119c-46c6-b75b-c56e64a26928",
-        })
+        {
+          Authorization: auth,
+        },
+        sendmessage
       );
     } catch (error) {
       console.log(error);
@@ -98,15 +106,19 @@ const Chat = () => {
   }
 
   function sendMessageFN() {
+    let auth = `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjb3PthqDtgbAiLCJleHAiOjE2NjIwOTMyMjksInVzZXJuYW1lIjoiS2FrYW9uYW1lMjQxMDkwMzEyOSJ9.Y9N_2aYxLDmglG50dZdd8XbXd1Xevp52Eu3sZ3GfzZ0TFLUZqKvR0Q0SPh35v7iJCMS17EFOHqKJdJFLAmdvdg`;
+
     try {
       stompClient.send(
         "/pub/chat/message",
-        {},
+        {
+          Authorization: auth,
+        },
         JSON.stringify({
           message: sendMessage,
           sender: sendNick,
-          type:"TALK",
-          roomId:"1d9af0fe-119c-46c6-b75b-c56e64a26928",
+          type: "TALK",
+          roomId: "22f09ce6-cb44-4f16-a054-20f8a09f5275",
         })
       );
     } catch (error) {
@@ -114,45 +126,49 @@ const Chat = () => {
     }
     // 채팅을 보낸다.
   }
-  const makeChatRoom=async()=>{
+  const makeChatRoom = async () => {
     const params = new URLSearchParams();
-    params.append('name', '11');
+    params.append("name", "11");
 
     await axios
-    .post(`http://52.79.223.9/chat/room`,params)
-    .then((response) => {
-      console.log("성공", response);
-      setChatRoomId(response.data.roomId)
-      console.log(response.data.roomId)
-      console.log(chatRoomId)
-    })
-    .catch((error) => {
-      console.log("에러", error);
-    });
-  }
-
+      .post(`http://52.79.223.9/chat/room`, params)
+      .then((response) => {
+        console.log("성공", response);
+        setChatRoomId(response.data.uuid);
+        console.log(response.data.uuid);
+        console.log(chatRoomId);
+      })
+      .catch((error) => {
+        console.log("에러", error);
+      });
+  };
 
   return (
     <div className="chat-wrap">
-      <div style={{position:"absolute",right:"349px",top:"70px"}}>
-      {/* <button onClick={disconnect}>소켓 연결 끊기!</button> */}
-      <button onClick={makeChatRoom}>채팅방생성</button>
-      {/* <hr></hr> */}
-      {/* <input  placeholder="chaatroom을 입력하세요" onChange={(e) => {
+      <div style={{ position: "absolute", right: "349px", top: "70px" }}>
+        {/* <button onClick={disconnect}>소켓 연결 끊기!</button> */}
+        <button onClick={makeChatRoom}>채팅방생성</button>
+        {/* <hr></hr> */}
+        {/* <input  placeholder="chaatroom을 입력하세요" onChange={(e) => {
           setPostId(e.target.value);
         }}></input> */}
-      <input
-        placeholder="nickname을 입력하세요"
-        onChange={(e) => {
-          setSendNick(e.target.value);
-        }}
-      ></input>
-      <button onClick={sendNicknameFN}>닉네임등록</button>
+        <input
+          placeholder="nickname을 입력하세요"
+          onChange={(e) => {
+            setSendNick(e.target.value);
+          }}
+        ></input>
+        <button onClick={sendNicknameFN}>닉네임등록</button>
       </div>
-      
-    
+
       <div className="chat-header-wrap">
-        <div className="chat-header-icon" style={{ marginLeft: "28px" }} onClick={()=>{navigate("/")}}>
+        <div
+          className="chat-header-icon"
+          style={{ marginLeft: "28px" }}
+          onClick={() => {
+            navigate("/");
+          }}
+        >
           <span
             className="material-symbols-outlined"
             style={{ fontSize: "28px" }}
@@ -161,22 +177,31 @@ const Chat = () => {
           </span>
         </div>
         <div className="chat-header-title">Lorem ipsum dolor...</div>{" "}
-        <div className="chat-header-icon" style={{ marginRight: "35px" }} onClick={modalHandler}>
-          <span className="material-icons-outlined" style={{ fontSize: "28px" }}>
+        <div
+          className="chat-header-icon"
+          style={{ marginRight: "35px" }}
+          onClick={modalHandler}
+        >
+          <span
+            className="material-icons-outlined"
+            style={{ fontSize: "28px" }}
+          >
             logout
-          </span> 
+          </span>
         </div>
       </div>
       <div ref={RefViewControll} className="chat-message-container">
-        <MessagelList showMessage={showMessage} sendNick={sendNick}/>
+        <MessagelList showMessage={showMessage} sendNick={sendNick} />
       </div>
       <div className="chat-input-wrap">
-       <MessageInput sendMessageFN={sendMessageFN} setSendMessage={setSendMessage} sendMessage={sendMessage}/>
+        <MessageInput
+          sendMessageFN={sendMessageFN}
+          setSendMessage={setSendMessage}
+          sendMessage={sendMessage}
+        />
       </div>
-      <MyModal isOpen={modalIsopen} handleClickCancel={handleClickCancel}/>
+      <MyModal isOpen={modalIsopen} handleClickCancel={handleClickCancel} />
     </div>
   );
 };
 export default Chat;
-
-
