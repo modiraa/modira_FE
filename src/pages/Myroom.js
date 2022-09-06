bb98d898a1b31a2acc824529babba3aaff41dd3a
ondoo <leh237@gmail.com>
2022-09-07 01:48:44 +0900

    register username 오류수정, myroom데이터확인

--- src/pages/Myroom.js
+++ src/pages/Myroom.js
@@ -1,57 +1,58 @@
 import styled from "styled-components";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams} from "react-router-dom";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import LowerNavbar from "../components/LowerNavbar";
 import Navbar from "../components/Navbar";
 import MiniBanner from "../image/MiniBanner.png";
-import "../css(subin)/MainBanner.css"
+import "../css(subin)/MainBanner.css";
+import Enter1 from "../image/Enter1.png";
 
 const MyRoom = () => {
   const navigate = useNavigate();
+  const params = useParams();
 
-  const [myData, setMyData] = useState({
+  const [data, setData] = useState({
     postId: "id",
     title: "Lorem ipsum dolor1",
-    menuForImage: "피자이미지",
+    menuForImage: { Enter1 },
     menu: "피자",
   });
 
-  const [joinData, setJoinData] = useState({
-    postId: "id",
-    title: "Lorem ipsum dolor2",
-    menuForImage: "치킨이미지",
-    menu: "치킨",
-  });
+  // const [joinData, setJoinData] = useState({
+  //   postId: "id",
+  //   title: "Lorem ipsum dolor2",
+  //   menuForImage: "치킨이미지",
+  //   menu: "치킨",
+  // });
 
   const getPost = async () => {
     const ACCESS_TOKEN = sessionStorage.getItem("token");
     console.log(ACCESS_TOKEN);
     //생성한 모임
+    // await axios
+    //   .get("http://3.34.129.164/api/myposts", {
+    //     headers: {
+    //       Authorization: ACCESS_TOKEN,
+    //     },
+    //   })
+    //   .then((response) => {
+    //     setData(response.data[0]);
+    //     console.log("데이터 나와랏", response.data[0]);
+    //   })
+    //   .catch((error) => {
+    //     console.log("에러", error);
+    //   });
+    // 참여한 모임
     await axios
-      .get("http://3.34.129.164/api/myposts", {
-        headers: {
-          Authorization: ACCESS_TOKEN,
-        },
-      })
-      .then((response) => {
-        setMyData(response.myData[0]);
-        console.log("데이터 나와랏", response.myData[0]);
-      })
-      .catch((error) => {
-        console.log("에러", error);
-      });
-    //참여한 모임
-    await axios
-
       .get("http://3.34.129.164/api/myjoin", {
         headers: {
           Authorization: ACCESS_TOKEN,
         },
       })
       .then((response) => {
-        setJoinData(response.joinData[0]);
-        console.log("데이터 나와랏", response.joinData[0]);
+        setData(response.data[0]);
+        console.log("데이터 나와랏", response.data[0]);
       })
       .catch((error) => {
         console.log("에러", error);
@@ -61,18 +62,18 @@ const MyRoom = () => {
     getPost();
   }, []);
 
-  // //모임 삭제
-  //   const removePost = async () => {
-  //     // const ACCESS_TOKEN = sessionStorage.getItem("token");
-  //     // console.log(ACCESS_TOKEN);
-  //     await axios
-  //       .delete(`http://3.34.129.164/api/post/${postId}`)
-  //       .then((response) => {
-  //         console.log("삭제");
-  //       })
-  //       .catch((error) => {
-  //         console.log("에러", error);
-  //       });}
+  //모임 삭제
+    const removePost = async () => {
+      // const ACCESS_TOKEN = sessionStorage.getItem("token");
+      // console.log(ACCESS_TOKEN);
+      await axios
+        .delete(`http://3.34.129.164/api/post/${params.postId}`)
+        .then((response) => {
+          console.log("삭제");
+        })
+        .catch((error) => {
+          console.log("에러", error);
+        });}
   return (
     <>
       <Navbar />
@@ -89,39 +90,65 @@ const MyRoom = () => {
             </p>
           </div>
         </Minibanner>
-        <CreateRoom>
+        {/* <CreateRoom>
           <p>생성한 모임</p>
           <Post>
-            {myData ? (
-              <div>
-                <img src={myData.menuForImage} />
-                <span className="material-symbols-outlined">delete</span>
+             {data ? ( 
+              <MakedRoom  onClick={() => {
+                  navigate("/postdetail:postId");
+                }}>
+
+               <img src={data.menuForImage} /> 
+               <span className="material-symbols-outlined">delete</span>
                 <p>게시물 삭제</p>
-              </div>
-            ) : (
-              <div>
-                <span className="material-symbols-outlined">add_circle</span>
-                <div>새 모임 만들러 가기</div>
-              </div>
-            )}
+              </MakedRoom>
+            ) : ( 
+              <ToMakeRoom
+              onClick={() => {
+                navigate("/write");
+              }}
+            >
+              <span
+                className="material-symbols-outlined"
+                style={{
+                  fontVariationSettings: "'FILL' 1",
+                }}
+              >
+                add_circle
+              </span>
+              <p>새 모임 만들러 가기</p>
+            </ToMakeRoom>
+            )} 
           </Post>
-          <Title>{myData.title}</Title>
+          <Title>{data?.title}</Title>
         </CreateRoom>
-        <BorderLine />
+        <BorderLine /> */}
         <CreateRoom>
           <p>신청한 모임</p>
           <Post>
-            {myData ? (
-              <div>새 모임 만들러 가기</div>
+            {data ? (
+              <ToMakeRoom
+                onClick={() => {
+                  navigate("/morepost");
+                }}
+              >
+                <span
+                  className="material-symbols-outlined"
+                  style={{
+                    fontVariationSettings: "'FILL' 1",
+                  }}
+                >
+                  add_circle
+                </span>
+                <p>모임 찾아보러 가기</p>
+              </ToMakeRoom>
             ) : (
-              <div>
-                <img src={joinData.menuForImage} />
-                <span className="material-symbols-outlined">delete</span>
-                <p>게시물 삭제</p>
-              </div>
+              <MakedRoom>
+                <img src={data?.menuForImage} />
+              </MakedRoom>
             )}
           </Post>
-          <Title>{joinData.title}</Title>
+          <Title>{data.title}</Title>
         </CreateRoom>
       </Container>
       <div className="main-wrap-lowernavbar">
@@ -136,7 +163,7 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  font-family: "AppleSDGothicNeoB00";
+  font-size: 26px;
   width: 525px;
   background-color: #fff;
   margin-bottom: 12px;
@@ -170,38 +197,54 @@ const CreateRoom = styled.div`
 const BorderLine = styled.div`
   width: 100%;
   height: 12px;
-  background: #f7f7f7; ;
+  background: #FFFCF6;
 `;
 
 const Post = styled.div`
   width: 439px;
   height: 167px;
-  background-color: #eaf5d3;
   border-radius: 12px;
   border: none;
   overflow: hidden;
+  background-color: #fffcf6;
   cursor: pointer;
 
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-  div {
-    position: absolute;
-    margin-left: 300px;
-    margin-top: 14px;
-    display: flex;
-    flex-direction: row;
+`;
+
+const MakedRoom = styled.div`
+ width: 439px;
+  height: 167px;
+  background-size: contain;
+  display: flex;
+  flex-direction: row;
     p {
       margin-left: 4px;
       font-size: 17px;
     }
     span {
-      margin: 0;
+      margin-left: 300px;
       font-size: 24px;
     }
+`;
+
+const ToMakeRoom = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 439px;
+  height: 167px;
+  border-radius: 12px;
+  border: none;
+  overflow: hidden;
+  padding-top: 10px;
+  span {
+    font-size: 25px;
+    color: #ffbb31;
   }
-
-  `;
-
+  p {
+    margin-top: 15px;
+    color: #ffbb31;
+    font-size: 20px;
+  }
+`;
