8b32f6cc231d25fa3919d703b34d11f0ed1298df
youdaehoon <107843412+youdaehoon@users.noreply.github.com>
2022-09-13 16:14:53 +0900

    useprofile component 분리

--- src/components/public/RouteList.js
+++ src/components/public/RouteList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {  Route, Routes } from "react-router-dom";
 import Chat from "../../pages/Chat";
 import Enter from "../../pages/Enter";
 import GoogleRedirect from "../../pages/GoogleRedirect";
